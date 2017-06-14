4a7bf83fd3da565c50383ba3d3843bdf64495ef3
dpwebco <info@gmail.com>
2017-06-14 18:56:44 -0500

    added error_reporter class

--- php/process-report.php
+++ php/process-report.php
@@ -5,3 +5,11 @@
  * Date: 06/14/2017
  * Time: 6:33 PM
  */
+
+class error_reporter {
+    public $error_id;
+    public function getErrorId() {
+        $this->error_id = $_POST['error_id'];
+        return $this->error_id;
+    }
+}
