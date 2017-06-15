03ef86ebc81206f79afac94ae3f517a1d9b4f45c
dpwebco <info@gmail.com>
2017-06-14 23:40:32 -0500

    refectored and created an alert object

--- js/alert.js
+++ js/alert.js
@@ -1,16 +1,45 @@
 /**
  * Created by Darrell on 06/14/2017.
  */
-$(document).ready(function() {
-    $('.close-btn').on('click', function() {
-        $(this).parent().remove();
-    });
+// $(document).ready(function() {
+//     $('.close-btn').on('click', function() {
+//         $(this).parent().remove();
+//     });
+//
+//     $('#alert-app-error').on('click', function () {
+//         $(this).parent().remove();
+//     });
+//
+//     $('#alert-announcement').on('click', function () {
+//         $(this).parent().remove();
+//     });
+// });
 
-    $('#alert-app-error').on('click', function () {
-        $(this).parent().remove();
-    });
+var alert = {
+    init: function () {
+        this.cacheDom();
+        this.handleEvents();
+    },
 
-    $('#alert-announcement').on('click', function () {
-        $(this).parent().remove();
-    });
-});
+    cacheDom: function() {
+        this.closeBtn = $('.close-btn');
+        this.alertAppError = $('#alert-app-error');
+        this.alertAnnouncement = $('#alert-announcement');
+    },
+
+    handleEvents: function() {
+        this.closeBtn.on('click', function() {
+            $(this).parent().remove();
+        });
+
+        this.alertAppError.on('click', function() {
+            $(this).parent.remove();
+        });
+
+        this.alertAnnouncement.on('click', function() {
+            $(this).parent().remove();
+        })
+    }
+};
+
+alert.init();
