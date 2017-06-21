94283c456dbd5512005227356365581c9229d03f
dpwebco <info@gmail.com>
2017-06-21 17:55:36 -0500

    added the central point for the jquery functionality

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Darrell on 06/21/2017.
+ */
+var theme = {
+    options: {
+        startShowing: true,
+        hideOnWindowLoad: true,
+        listenOn: event
+    },
+    init: function () {
+
+    }
+};
+
+theme.init();
